b4d827b62fd9d5b55b81db1a935afb8b61ba91ec
Angelos Chalaris <chalarangelo@gmail.com>
2025-03-04 19:42:35 +0200

    Add exports

--- src/index.js
+++ src/index.js
@@ -1,1 +1,75 @@
-export default {};
+import readEx from './readEx.js';
+import { backReference } from './backReference.js';
+import {
+  captureGroup,
+  nonCaptureGroup,
+  namedGroup,
+  concat,
+  or,
+} from './group.js';
+import {
+  lookahead,
+  lookbehind,
+  negativeLookahead,
+  negativeLookbehind,
+} from './lookAround.js';
+import {
+  zeroOrOne,
+  zeroOrOneLazy,
+  oneOrMore,
+  oneOrMoreLazy,
+  zeroOrMore,
+  zeroOrMoreLazy,
+  repeat,
+  repeatLazy,
+} from './quantifiers.js';
+import {
+  startOfLine,
+  endOfLine,
+  wordBoundary,
+  nonWordBoundary,
+  digit,
+  nonDigit,
+  wordCharacter,
+  nonWordCharacter,
+  whitespaceCharacter,
+  nonWhitespaceCharacter,
+  anyCharacter,
+  anything,
+  something,
+} from './sequences.js';
+
+export {
+  readEx,
+  backReference,
+  captureGroup,
+  nonCaptureGroup,
+  namedGroup,
+  concat,
+  or,
+  lookahead,
+  lookbehind,
+  negativeLookahead,
+  negativeLookbehind,
+  zeroOrOne,
+  zeroOrOneLazy,
+  oneOrMore,
+  oneOrMoreLazy,
+  zeroOrMore,
+  zeroOrMoreLazy,
+  repeat,
+  repeatLazy,
+  startOfLine,
+  endOfLine,
+  wordBoundary,
+  nonWordBoundary,
+  digit,
+  nonDigit,
+  wordCharacter,
+  nonWordCharacter,
+  whitespaceCharacter,
+  nonWhitespaceCharacter,
+  anyCharacter,
+  anything,
+  something,
+};
